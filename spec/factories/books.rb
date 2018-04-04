e9d5c03856b84441d7e964f2c7a29b9426cc8bdf
redwarewolf <jarapedro96@gmail.com>
2018-04-04 17:38:41 -0300

    Book model tests

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require 'faker'
 
 FactoryBot.define do
@@ -8,5 +9,15 @@ FactoryBot.define do
     image      Faker::String.random(8)
     title      Faker::Lovecraft.tome
     publisher  Faker::Educator.university
+=======
+FactoryBot.define do
+  factory :book do
+    genre 'genre'
+    year       'year'
+    author     'author'
+    image      'image'
+    title      'title'
+    publisher  'publisher'
+>>>>>>> 7ff53b0... Book model tests
   end
 end
