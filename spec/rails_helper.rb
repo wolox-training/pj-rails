bec3309420636a443d6de834a730987a7b83c07f
redwarewolf <jarapedro96@gmail.com>
2018-04-04 17:38:41 -0300

    Book model tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,10 +5,6 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-<<<<<<< HEAD
-
-=======
->>>>>>> 207b6f4... WIP
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -34,10 +30,7 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-<<<<<<< HEAD
   config.include FactoryBot::Syntax::Methods
-=======
->>>>>>> 207b6f4... WIP
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
@@ -63,7 +56,6 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
-<<<<<<< HEAD
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
@@ -73,5 +65,3 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
-=======
->>>>>>> 207b6f4... WIP
