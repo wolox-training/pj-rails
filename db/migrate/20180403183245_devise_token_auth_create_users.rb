01e7b79977820c39e5b9e4a6f996a41fe1919e6f
redwarewolf <jarapedro96@gmail.com>
2018-04-05 14:42:06 -0300

    Removed commented code

--- db/migrate/20180403183245_devise_token_auth_create_users.rb
+++ db/migrate/20180403183245_devise_token_auth_create_users.rb
@@ -7,11 +7,6 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.1]
     ## Recoverable
     add_column :users, :allow_password_change, :boolean, :default => false
 
-    ## Lockable
-    # t.integer  :failed_attempts, :default => 0, :null => false # Only if lock strategy is :failed_attempts
-    # t.string   :unlock_token # Only if unlock strategy is :email or :both
-    # t.datetime :locked_at
-
     ## User Info
     add_column :users, :image, :string
 
@@ -20,6 +15,5 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.1]
 
 
     add_index :users, [:uid, :provider],     unique: true
-    # add_index :users, :unlock_token,       unique: true
   end
 end
