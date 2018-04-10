114cbfe378682c413027a8ff51135da7038ed1e8
redwarewolf <jarapedro96@gmail.com>
2018-04-10 13:11:38 -0300

    Removed posts from schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180405162520) do
+ActiveRecord::Schema.define(version: 20180403202736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,24 +24,6 @@ ActiveRecord::Schema.define(version: 20180405162520) do
     t.string "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "rents", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "book_id"
-    t.date "from"
-    t.date "to"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["book_id"], name: "index_rents_on_book_id"
-    t.index ["user_id"], name: "index_rents_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -69,6 +51,4 @@ ActiveRecord::Schema.define(version: 20180405162520) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
-  add_foreign_key "rents", "books"
-  add_foreign_key "rents", "users"
 end
