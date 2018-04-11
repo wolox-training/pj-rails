7656d8c5390ee5d6c86f4e8ba808f5231a07b18b
redwarewolf <jarapedro96@gmail.com>
2018-04-11 13:33:16 -0300

    fixed rent factory and user has many relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :first_name, :last_name, presence: true
+  has_many :rents, :dependent :destroy
 end
