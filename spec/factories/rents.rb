19cb33b0687e98657399261ae986bb1a628e0480
redwarewolf <jarapedro96@gmail.com>
2018-04-11 17:04:33 -0300

    Fixed tests

--- spec/factories/rents.rb
+++ spec/factories/rents.rb
@@ -1,15 +1,8 @@
 FactoryBot.define do
   factory :rent do
-<<<<<<< HEAD
-    user  nil
-    book  nil
-    from  Faker::Date.backward(14)
-    to    Faker::Date.forward(14)
-=======
     user
-    book 
+    book
     from { Faker::Date.backward(14) }
     to   { Faker::Date.forward(14) }
->>>>>>> 939f010833462647cddd94c5385e4bb77cd007c9
   end
 end
