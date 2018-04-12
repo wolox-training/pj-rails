bd08fbe4b6db3c89f4aa1f1962ec15274e874710
redwarewolf <jarapedro96@gmail.com>
2018-04-12 13:33:05 -0300

    fixed factories and removed comments

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    first_name            Faker::Name.first_name
-    last_name             Faker::Name.last_name
-    email                 Faker::Internet.email
-    password              '123456'
-    password_confirmation '123456'
+    first_name            { Faker::Name.first_name }
+    last_name             { Faker::Name.last_name }
+    email                 { Faker::Internet.email }
+    password              { Faker::String.random(8) }
+    password_confirmation { password.to_s }
   end
 end
