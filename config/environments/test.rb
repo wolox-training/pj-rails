031825032aea65e721fd51485d86d4d3f18aa961
redwarewolf <jarapedro96@gmail.com>
2018-04-16 14:41:45 -0300

    Added host

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  # config.action_mailer.default_url_options = {:host => "localhost:3000"}
+  config.action_mailer.default_url_options = {:host => "localhost"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :authentication => Rails.application.secrets.mailer_authentication,
