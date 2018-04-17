36d4f97426dba35574decfab1c953eef90c57e29
redwarewolf <jarapedro96@gmail.com>
2018-04-17 15:08:41 -0300

    fixed controller

--- spec/controllers/api/v1/book_suggestions_controller_spec.rb
+++ spec/controllers/api/v1/book_suggestions_controller_spec.rb
@@ -1,32 +1,32 @@
 require 'rails_helper'
 
-describe Api::V1::BooksuggestionsController, type: :controller do
+describe Api::V1::BookSuggestionsController, type: :controller do
   include_context 'Authenticated User'
   describe 'POST #create' do
     context 'When creating a valid user booksuggestion' do
-      let!(:new_booksuggestion_attributes) { attributes_for(:booksuggestion, user_id: user.id) }
+      let!(:new_book_suggestion_attributes) { attributes_for(:book_suggestion, user_id: user.id) }
       it 'creates a new rent' do
         expect do
-          post :create, params: { booksuggestion: new_booksuggestion_attributes }
-        end.to change { Booksuggestion.count }.by(1)
+          post :create, params: { book_suggestion: new_book_suggestion_attributes }
+        end.to change { BookSuggestion.count }.by(1)
       end
 
       it 'responds with 201 status' do
-        post :create, params: { booksuggestion: new_booksuggestion_attributes }
+        post :create, params: { book_suggestion: new_book_suggestion_attributes }
         expect(response).to have_http_status(:created)
       end
     end
 
     context 'When creating an invalid user booksuggestion' do
-      let!(:new_booksuggestion_attributes) { attributes_for(:booksuggestion, user_id: user.id, title: nil) }
+      let!(:new_book_suggestion_attributes) { attributes_for(:book_suggestion, user_id: user.id, title: nil) }
       before do
-        post :create, params: { booksuggestion: new_booksuggestion_attributes }
+        post :create, params: { book_suggestion: new_book_suggestion_attributes }
       end
 
       it 'doesn\'t create a new booksuggestion' do
         expect do
-          post :create, params: { booksuggestion: new_booksuggestion_attributes }
-        end.to change { Booksuggestion.count }.by(0)
+          post :create, params: { book_suggestion: new_book_suggestion_attributes }
+        end.to change { BookSuggestion.count }.by(0)
       end
 
       it 'responds with 422 status' do
